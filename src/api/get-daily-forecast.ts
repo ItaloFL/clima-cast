21df46ffe6fcf298a2cb651451617fc1d0bd3e2c
Italo Ferreira <italofl.dev@gmail.com>
2025-08-28 20:18:36 -0500

    feat :sparkles: skeleton component and alter type geolocation

--- src/api/get-daily-forecast.ts
+++ src/api/get-daily-forecast.ts
@@ -2,12 +2,12 @@ import { env } from "@/env";
 import { weatherAPI } from "@/lib/axios";
 
 interface GetDailyForecastRequest {
-  lat: number;
-  long: number;
+  latitude: number;
+  longitude: number;
 }
 
-export async function GetDailyForecast({ lat, long }: GetDailyForecastRequest) {
-  const response = await weatherAPI.get(`/forecast.json?key=${env.VITE_API_WEATHER_KEY}&q=${lat},${long}&days=1`);
+export async function GetDailyForecast({ latitude, longitude }: GetDailyForecastRequest) {
+  const response = await weatherAPI.get(`/forecast.json?key=${env.VITE_API_WEATHER_KEY}&q=${latitude},${longitude}&days=1`);
 
   return response.data
 }
